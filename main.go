45d6647d2bc52b5488ada09238c43d4ebb0c0bc3
DmitriyLewen <Lewendmitriy.lewen@smartforce.io>
2021-09-27 16:49:45 +0600

    b10

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 	fmt.Println(os.Getenv(envvars.PemData))
 	fmt.Println(os.Getenv(envvars.PemPathVariable))
 	fmt.Println(os.Getenv(envvars.GoPath))
-	fmt.Println("b9")
+	fmt.Println("b10")
 }
