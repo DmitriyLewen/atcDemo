73fb9fae1683fae3d45c513c2eb5786093e89bd2
DmitriyLewen <Lewendmitriy.lewen@smartforce.io>
2021-09-29 16:52:37 +0600

    grable

--- main.go
+++ main.go
@@ -7,18 +7,21 @@ import (
 )
 
 func main() {
-	fileString := "npm config set init.author.name \"Hiro Protagonist\" \nnpm config set init.author.email \"hiro@showcrash.io\" \nnpm config set init.author.url \"http://hiro.snowcrash.io\" \nnpm config set init.license \"MIT\" \nnpm config set init.version \"0.0.1\""
-	findnpmrc(fileString)
-	bytes, err := ioutil.ReadFile(".npmrc")
-	s := string(bytes)
+	bytes, err := ioutil.ReadFile("grable.properties")
 	if err != nil {
 		fmt.Println("error read file: ", err)
 	}
-	fmt.Println("11:", s)
+	s := string(bytes)
+	findnpmrc(s)
 }
 
 func findnpmrc(fileString string) {
-	regex, _ := regexp.Compile("npm config set init.version \"([^\t\n\f\r]*?)\"")
+	regex, _ := regexp.Compile("version=([^\t\n\f\r]*)")
 	res := regex.FindStringSubmatch(fileString)
-	fmt.Println(res[1])
+	fmt.Println(res)
+	if len(res) != 0 {
+		fmt.Println("____________")
+		fmt.Println(res[1])
+	}
+
 }
