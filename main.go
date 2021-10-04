34f0cc2e4697f9bd50ad55ffbf589d49188d28cf
DmitriyLewen <dmitriy.lewen@smartforce.io>
2021-10-04 15:47:34 +0600

    1

--- main.go
+++ main.go
@@ -8,6 +8,38 @@ import (
 )
 
 func main() {
+
+	var str1 struct {
+		name string
+		id   int
+	}
+
+	var str2 struct {
+		name string
+		id   int
+	}
+
+	str1.name = "vadim"
+	str2.id = 1
+
+	str2.name = ""
+	str2.id = 2
+
+	var names = &str1.name
+	str2.name = *names
+	fmt.Println("str1: ", str1.name, "    str2: ", str2.name, "    names: ", *names)
+	str1.name = ""
+	fmt.Println("str1: ", str1.name, "    str2: ", str2.name, "    names: ", *names)
+	var str1_name = &str1.name
+	str1.name = ""
+	fmt.Println(*str1_name)
+	fmt.Println(str1.name)
+
+	str1.name = "str1 name"
+	str1.id = 1
+
+	str2.name = str1.name
+
 	bytes, err := ioutil.ReadFile("grable.properties")
 	if err != nil {
 		fmt.Println("error read file: ", err)
