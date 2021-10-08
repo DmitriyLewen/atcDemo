e8aa89a16557141f0af81db3bf135d862b1bfd66
DmitriyLewen <dmitriy.lewen@smartforce.io>
2021-10-08 13:53:56 +0600

    m

--- main.go
+++ main.go
@@ -2,68 +2,107 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"path/filepath"
 	"regexp"
 	"strings"
 )
 
 func main() {
 
-	var str1 struct {
-		name string
-		id   int
-	}
+	str1 := "Cjxwcm9qZWN0IHhtbG5zPSJodHRwOi8vbWF2ZW4uYXBhY2hlLm9yZy9QT00vNC4wLjAiIHhtbG5zOnhzaT0iaHR0cDovL3d3dy53My5vcmcvMjAwMS9YTUxTY2hlbWEtaW5zdGFuY2UiCgl4c2k6c2NoZW1hTG9jYXRpb249Imh0dHA6Ly9tYXZlbi5hcGFjaGUub3JnL1BPTS80LjAuMCBodHRwOi8vbWF2ZW4uYXBhY2hlLm9yZy94c2QvbWF2ZW4tNC4wLjAueHNkIj4KCTx2ZXJzaW9uPjU8L3ZlcnNpb24"
+	str2 := "Cjxwcm9qZWN0IHhtbG5zPSJodHRwOi8vbWF2ZW4uYXBhY2hlLm9yZy9QT00vNC4wLjAiIHhtbG5zOnhzaT0iaHR0cDovL3d3dy53My5vcmcvMjAwMS9YTUxTY2hlbWEtaW5zdGFuY2UiCgl4c2k6c2NoZW1hTG9jYXRpb249Imh0dHA6Ly9tYXZlbi5hcGFjaGUub3JnL1BPTS80LjAuMCBodHRwOi8vbWF2ZW4uYXBhY2hlLm9yZy94c2QvbWF2ZW4tNC4wLjAueHNkIj4KCTx2ZXJzaW9uPjU8L3ZlcnNpb24"
+	fmt.Println("compare string :", str1 == str2)
 
-	var str2 struct {
-		name string
-		id   int
-	}
+	url1 := "https://api.github.com/repos/Codertocat/Hello-World/contents/contents/pom.xml"
+	url2 := "https://api.github.com/repos/Codertocat/Hello-World/contents/contents/pom.xml?ref=6113728f27ae82c7b1a177c8d03f9e96e0adf246"
+	fmt.Println("regexghservise url1: ", regexghserviseOld(url1))
+	fmt.Println("regexghservise url2: ", regexghserviseOld(url2))
+	fmt.Println("regexghservise url1: ", regexghserviseNew(url1))
+	fmt.Println("regexghservise url2: ", regexghserviseNew(url2))
 
-	str1.name = "vadim"
-	str2.id = 1
+	strm := madeСaptionToTemplate(`{{.version}}Vte`, "5")
+	fmt.Println("strm: ", strm)
 
-	str2.name = ""
-	str2.id = 3
+	regStr := "([^\t\n\f\r]*?)$"
+	fmt.Println("path " + regStr)
 
-	var names = &str1.name
-	str2.name = *names
-	fmt.Println("str1: ", str1.name, "    str2: ", str2.name, "    names: ", *names)
-	str1.name = ""
-	fmt.Println("str1: ", str1.name, "    str2: ", str2.name, "    names: ", *names)
-	var str1_name = &str1.name
-	str1.name = ""
-	fmt.Println(*str1_name)
-	fmt.Println(str1.name)
+	fmt.Println("contents/pom.xml : ", filepath.Base(`project//contents/pom.xml`))
 
-	str1.name = "str1 name"
-	str1.id = 1
+	fmt.Println("contents/pom.xml/ : ", filepath.Base("contents/pom.xml/"))
+	// if fetcher == nil {
+	// 	log.Printf("Error: non support Path = %q", settings.Path)
+	// 	return
+	// }
 
-	str2.name = str1.name
+	// regStr := "([^\t\n\f\r]*)"
+	// regexPath, err := regexp.Compile("path: " + regStr)
+	// if err != nil {
+	// 	return err
+	// }
+	// regexTemplate, _ := regexp.Compile("template: " + regStr)
+	// if err != nil {
+	// 	return err
+	// }
+	// regexBehavior, _ := regexp.Compile("behavior: " + regStr)
+	// if err != nil {
+	// 	return err
+	// }
+	// path := regexPath.FindStringSubmatch(string(content))
+	// template := regexTemplate.FindStringSubmatch(string(content))
+	// behavior := regexBehavior.FindStringSubmatch(string(content))
+	// if len(path) > 0 {
+	// 	atcSettingsPtr.Path = path[1]
+	// }
+	// if len(template) > 0 {
+	// 	atcSettingsPtr.Template = template[1]
+	// }
+	// if len(behavior) > 0 {
+	// 	atcSettingsPtr.Behavior = behavior[1]
+	// }
+	// //err := yaml.Unmarshal([]byte(content), atcSettingsPtr)
+	// return nil
 
-	bytes, err := ioutil.ReadFile("grable.properties")
-	if err != nil {
-		fmt.Println("error read file: ", err)
-	}
-	s := string(bytes)
-	vers := findnpmrc(s)
-	templateVers := "v{{.version}}"
-	v := "1.0"
-	result := strings.Replace(templateVers, "{{.version}}", vers, -1)
-	fmt.Println(result)
-	fmt.Println("v" + v)
-
-	var mape = map[string]string{
-		"1": "1",
-		"2": "2",
-		"3": "3",
-	}
-
-	fmt.Println(mape["1"])
-	fmt.Println(mape["5"])
+	// {`template: v{{.version}}`, `Added a new version for "Codertocat/Hello-World": "v5"`, `v5`},
+	// 	{`template: vTest{{.version}}`, `Added a new version for "Codertocat/Hello-World": "vTest5"`, `vTest5`},
+	// 	{`template: {{.version}}Vte`, `Added a new version for "Codertocat/Hello-World": "5Vte"`, `5Vte`},
+	// 	{`template: vVv{.version}`, `Added a new version for "Codertocat/Hello-World": "v5"`, `v5`},
+	// 	{`template: `, `Added a new version for "Codertocat/Hello-World": "v5"`, `v5`},
+	// 	{`template: v{{.version}}`, `Added a new version for "Codertocat/Hello-World": "v5"`, `v5`},
+	// 	{`template: vVv{{.verson}}`, `Added a new version for "Codertocat/Hello-World": "v5"`, `v5`},
+	// 	{`template: vvV{{version}}`, `Added a new version for "Codertocat/Hello-World": "v5"`, `v5`},
 }
 
+func madeСaptionToTemplate(template, version string) string {
+	if !strings.Contains(template, "{{.version}}") {
+		return "v" + version
+	}
+	return strings.Replace(template, "{{.version}}", version, -1)
+}
+
+func regexghserviseOld(url string) bool {
+	matched, err := regexp.MatchString("pom\\.xml\\?ref=", url)
+	if err != nil {
+		return false
+	}
+	return matched
+}
+
+func regexghserviseNew(url string) bool {
+	matched, err := regexp.MatchString("pom\\.xml$", url)
+	if err != nil {
+		return false
+	}
+	return matched
+}
 func findnpmrc(fileString string) string {
-	regex, _ := regexp.Compile("version=([^\t\n\f\r]*)")
+	reg := `contents/pom.xml|gradle.properties|.npmrc`
+	regex, _ := regexp.Compile(reg)
+	str1 := `gradle.properties`
+	str2 := `contents/pom.xml`
+	matched, _ := regexp.MatchString(reg, str1)
+	fmt.Println(matched)
+	matched, _ = regexp.MatchString(reg, str2)
+	fmt.Println(matched)
 	res := regex.FindStringSubmatch(fileString)
 	if len(res) != 0 {
 		return res[1]
