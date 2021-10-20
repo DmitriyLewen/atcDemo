2337485d009e5a265948bca7182175f8bd26fe50
DmitriyLewen <dmitriy.lewen@smartforce.io>
2021-10-20 14:26:48 +0600

    test

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 	fmt.Println("regexghservise url2: ", regexghserviseNew(url2))
 
 	strm := madeСaptionToTemplate(`{{.version}}Vtee`, "5")
-	fmt.Println("strm: ", strm)
+	fmt.Println("strm:  ", strm)
 
 	regStr := "([^\t\n\f\r]*?)$"
 	fmt.Println(" path " + regStr)
